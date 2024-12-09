1e8812b021354424e5b7657646a3a9599ee581a4
Cristiana Tabacaru <cris@Cristianas-MacBook-Pro.local>
2024-12-10 00:23:42 +0200

    Added Sign Up form

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
