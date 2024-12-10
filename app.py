9345703efcd005a760f10891cef09e079afa499f
Cristiana Tabacaru <cris@Cristianas-MacBook-Pro.local>
2024-12-10 17:55:43 +0200

    fetched data from my teammate's branch

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
@@ -7,13 +7,61 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Get form data
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT password FROM users WHERE email = ?", (email,))
+        db_password = cursor.fetchone()
+        connection.commit()
+        connection.close()
+        if db_password:
+            if password == db_password[0]:
+                return redirect(url_for('success'))
+            else:
+                print("incorrect password")
+                return redirect(url_for('login'))
+        else:
+            print("incorrect mail")
+            return redirect(url_for('login'))
+            
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        # Extract data from the form
+        name = request.form.get('name')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        age = request.form.get('age')
+        gender = request.form.get('gender')
+        height = request.form.get('height')
+        weight = request.form.get('weight')
+        goal = request.form.get('goal')
+        agree = request.form.get('agree')
+
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO users (name, username, email, password, age, gender, caloric_goal, height, weight)\
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (name, username, email, password, age, gender, goal, height, weight))
+        connection.commit()
+        connection.close()
+
+        # Redirect to a success page or render a response
+        return redirect(url_for('success'))
+    return render_template('register.html')  # Render the form template
+
+# asta nush daca e necesara
+@app.route('/success')
+def success():
+    return "Registration Successful!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
